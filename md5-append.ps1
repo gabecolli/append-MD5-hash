d99269e318ee8c9086e188a99e99308ffce39c6e
Gabriel <80380712+gabecolli@users.noreply.github.com>
2023-03-07 15:08:55 -0600

    Update md5-append.ps1
    CHANGED MD5 TO SHA256

--- md5-append.ps1
+++ md5-append.ps1
@@ -1,4 +1,4 @@
-$folderPath = "filepath"
+$folderPath = "C:\Users\gabrielcolli\test-md5-folder"
 $filter = "*.*"
 
 $watcher = New-Object System.IO.FileSystemWatcher
@@ -9,21 +9,21 @@ $watcher.EnableRaisingEvents = $true
 
 $onChange = Register-ObjectEvent $watcher "Created" -Action {
     $filePath = $Event.SourceEventArgs.FullPath
-    $md5 = (Get-FileHash $filePath -Algorithm MD5).Hash
+    $md5 = (Get-FileHash $filePath -Algorithm SHA256).Hash
     $newFilePath = $filePath -replace "\.([^\.]+)$", "_$md5.`$1"
     Rename-Item $filePath -NewName $newFilePath
 }
 
 $onChange = Register-ObjectEvent $watcher "Changed" -Action {
     $filePath = $Event.SourceEventArgs.FullPath
-    $md5 = (Get-FileHash $filePath -Algorithm MD5).Hash
+    $md5 = (Get-FileHash $filePath -Algorithm SHA256).Hash
     $newFilePath = $filePath -replace "\.([^\.]+)$", "_$md5.`$1"
     Rename-Item $filePath -NewName $newFilePath
 }
 
 $onChange = Register-ObjectEvent $watcher "Renamed" -Action {
     $filePath = $Event.SourceEventArgs.FullPath
-    $md5 = (Get-FileHash $filePath -Algorithm MD5).Hash
+    $md5 = (Get-FileHash $filePath -Algorithm SHA256).Hash
     $newFilePath = $filePath -replace "\.([^\.]+)$", "_$md5.`$1"
     Rename-Item $filePath -NewName $newFilePath
 }
